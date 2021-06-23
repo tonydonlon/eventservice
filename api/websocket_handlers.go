d22336e4427d5d6aa33d262f363d5b323c021da1
Tony Donlon <tony.donlon@gmail.com>
2021-06-22 19:45:26 -0500

    add a WS closehandler

--- api/websocket_handlers.go
+++ api/websocket_handlers.go
@@ -3,6 +3,7 @@ package api
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
@@ -20,11 +21,31 @@ func WebsocketHandler(wr EventWriter) http.HandlerFunc {
 		}
 		defer c.Close()
 
+		// should closehandler send sessionEnd?
+		// depends if the client can send end before dropping (like on browsers .onbeforeunload event)
+
+		// TODO make this not hacky... use mutex lock. ensure one session END can be written
+		sessionEndSent := false
+		c.SetCloseHandler(func(code int, text string) error {
+			log.Infof("Invoking CloseHandler code: %s, text: %s", code, text)
+			if !sessionEndSent {
+				endEvent := SessionEvent{
+					Timestamp: int(time.Now().UnixNano() / 1000000),
+					Type:      EndSession,
+					SessionID: sessionID,
+				}
+				err = wr.Write(endEvent, sessionID)
+				if err != nil {
+					log.Error("session end write:", err)
+					return err
+				}
+			}
+			return nil
+		})
+
 		for {
 			var message []SessionEvent
 			err = c.ReadJSON(&message)
-
-			// TODO closehandler send sessionEND
 
 			// TODO message validation;missing values, etc.
 			if err != nil {
@@ -38,8 +59,9 @@ func WebsocketHandler(wr EventWriter) http.HandlerFunc {
 
 			// SESSION_START is guaranteed to be first and SESSION_END is guaranteed to be last
 			// TODO how to be defensive if that is not the case
-			log.Warn("----------RECEIVED %d messages", len(message))
-			// TODO create event writer bus that has message channel per session
+
+			// TODO this should handel serial nature of the messages
+			// but it would be better to explicitly control that ...create event writer bus that has message channel per session
 			for _, evt := range message {
 				switch evt.Type {
 				case StartSession:
@@ -61,13 +83,21 @@ func WebsocketHandler(wr EventWriter) http.HandlerFunc {
 						log.Error("write:", err)
 					}
 				case EndSession:
+					sessionEndSent = true
 					err = wr.Write(evt, sessionID)
 					if err != nil {
 						log.Error("write:", err)
 					}
 					log.Info("session ended: closing connection")
-					// TODO send message "bye"
-					c.Close()
+
+					// not sure about the etiquette here
+					msg := struct {
+						Message string `json:"message"`
+					}{
+						"bye",
+					}
+					c.WriteJSON(msg)
+					// c.Close() is deferred
 					return
 				default:
 					// TODO there needs to be an error channel for the async version of this
